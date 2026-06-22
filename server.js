a0103482e829b288f533ed88273bce1d3726da28
Areeba <honeyareeba34@gmail.com>
2026-06-22 22:10:17 +0500

    Fix Shopify CORS

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
+app.options("*", cors());
 
 /* =====================
    FETCH SHOPIFY PRODUCTS
