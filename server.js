7f5dc7176d3fbf320bb2c9ab7930b85b3a4ae380
Areeba <honeyareeba34@gmail.com>
2026-06-23 03:17:25 +0500

    final

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ dotenv.config();
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 app.get("/test-env", (req, res) => {
   res.json({
     groq: !!process.env.GROQ_API_KEY
